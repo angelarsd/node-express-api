dfc16137965f9e5d46a695e27c4828ca5c1a3713
Ángel Silva <angel.silva@tunubi.com>
2025-01-06 12:18:10 -0300

    Update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require("dotenv").config();
+const { Client } = require("pg");
 
 const app = express();
 
@@ -6,8 +8,40 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('¡Hola, esta es una API básica en Node.js!');
+const CONFIG_DB = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true, // Configuración para conexiones SSL
+  },
+};
+
+const db = new Client(CONFIG_DB);
+
+app.use(async (req, res, next) => {
+  try {
+    if (!db._connected) {
+      await db.connect();
+      db._connected = true;
+    }
+    next();
+  } catch (err) {
+    console.error("Error conectando a la base de datos:", err);
+    res.status(500).send("Error conectando a la base de datos");
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await db.query("SELECT * from test");
+    res.send(`Resultados de la consulta: ${JSON.stringify(result.rows)}`);
+  } catch (err) {
+    console.error("Error ejecutando la consulta:", err);
+
+  }
 });
 
 app.get('/data', (req, res) => {
@@ -21,4 +55,12 @@ app.post('/data', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+  console.log("Cerrando la conexión...");
+  if (db._connected) {
+    await db.end();
+  }
+  process.exit();
 });
