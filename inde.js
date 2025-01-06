058b2687be5e39bcb2136c32172b74b50824f9e1
Ángel Silva <angel.silva@tunubi.com>
2025-01-06 10:39:52 -0300

    initial commit

---
+++ inde.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, esta es una API básica en Node.js!');
+});
+
+app.get('/data', (req, res) => {
+  res.json({ message: 'Esta es la ruta /data', data: [1, 2, 3] });
+});
+
+app.post('/data', (req, res) => {
+  const newData = req.body;
+  res.status(201).json({ message: 'Datos recibidos', data: newData });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
